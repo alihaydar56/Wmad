fba4fa733002b1e9b4b924a64a8ad1cb57da059b
Gathaus <rizamertyagci@gmail.com>
2020-11-23 16:06:49 +0300

    Footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,18 +5,31 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<>
-				<footer className="footer pt-2">
+				<footer className="footer pt-2 ">
 					<div className="container mt-2">
 						<div className="row d-flex justify-content-between">
-							<div className="col-md-4 footer-links text-center">
-								<div className="container">
-									<a href="#" target="_blank" className="">Github</a>
-									<a href="#" target="_blank">Instagram</a>
-									<a href="#" target="_blank">Linkedin</a>
-								</div>
+							<div className="col-md-6 text-left footer-text-container">
+								<p className="footer-text"> © 2020 reserved by Wmad</p>
 							</div>
-							<div className="col-md-5 text-right footer-text-container">
-								<p className="footer-text">  © 2020 All rights reserved by Rıza Mert Yağcı</p>
+							<div className="col-md-4 footer-links text-right">
+								<div className="container">
+									<a
+										href="#"
+										target="_blank"
+										className=""
+									>
+										Github
+									</a>
+									<a
+										href="#"
+										target="_blank"
+									>
+										Instagram
+									</a>
+									<a href="#" target="_blank">
+										Linkedin
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
